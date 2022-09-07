c0bff2349d4a158363d4f0b354ee0903540d5516
Muhammed Shittu <coolzyte@gmail.com>
2022-09-07 10:03:36 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import { logo } from "../assets";
+import { logo } from "../assets/images";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="container">
-        <a className="inline-block" href="/">
-          <img src={logo} alt="clout zap" className="h-8" />
+    <nav className="navbar fixed-top">
+      <div className="container mx-auto sm:px-4 lg:px-8 flex flex-wrap items-center justify-between lg:flex-nowrap">
+        <a
+          className="inline-block mr-4 py-0.5 text-xl whitespace-nowrap hover:no-underline focus:no-underline"
+          href="/"
+        >
+          <img src={logo} alt="pato" className="h-8" />
         </a>
+
+        <button
+          className="background-transparent rounded text-xl leading-none hover:no-underline focus:no-underline lg:hidden lg:text-gray-400"
+          type="button"
+          data-toggle="offcanvas"
+        >
+          <span className="navbar-toggler-icon inline-block w-8 h-8 align-middle"></span>
+        </button>
       </div>
     </nav>
   );
---
+++ src/constants/index.js
@@ -0,0 +1,33 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "details",
+    title: "Details",
+  },
+  {
+    id: "pricing",
+    title: "Pricing",
+  },
+];
+
+export const dropDown = [
+  {
+    id: "article",
+    title: "Article",
+  },
+  {
+    id: "terms",
+    title: "Terms",
+  },
+  {
+    id: "privacy",
+    title: "Privacy",
+  },
+];
